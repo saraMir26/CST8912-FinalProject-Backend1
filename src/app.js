7a501e8533aab6deb7f191ca6fd86694ddf0a585
saraMir26 <mirz0014@algonquinlive.com>
2026-04-16 00:27:08 -0400

    Fix backend CORS config

--- src/app.js
+++ src/app.js
@@ -2,25 +2,32 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-const app = express();
-
-app.use(cors({
-  origin: process.env.FRONTEND_URL || "*",
-  credentials: true
-}));
-app.options("*", cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
-
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const postRoutes = require("./routes/postRoutes");
 const followRoutes = require("./routes/followRoutes");
 const uploadRoutes = require("./routes/uploadRoutes");
+
+const app = express();
+
+const allowedOrigin =
+  "https://cst8912-final-chatbox-erh6d2gechfmh0h0.canadacentral-01.azurewebsites.net";
+
+app.use(
+  cors({
+    origin: allowedOrigin,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
